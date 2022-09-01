9cc007d65a1972d034694d7620a922b511e89290
Jaqueline <jaqueline_lessa@outlook.com>
2022-08-31 21:30:13 -0300

    Adição de Animações

---
+++ Scripts/ScrollReveal.js
@@ -0,0 +1,48 @@
+ScrollReveal().reveal('.revelar', {
+    origin: "bottom",
+    distance: "50px",
+    delay: 400,
+    reset: false
+} );
+
+ScrollReveal().reveal('.tituloRevelar',{
+    origin: "left",
+    distance: "50px",
+    delay: 200,
+    reset: false,
+})
+
+ScrollReveal().reveal('.boxDestinos', {
+    origin: "bottom",
+    distance: "50px",
+    delay: 500,
+    reset: false
+})
+
+ScrollReveal().reveal('.cardHover', {
+    origin: "bottom",
+    distance: "50px",
+    delay: 500,
+    reset: false
+})
+
+ScrollReveal().reveal('#boxForm', {
+    origin: "bottom",
+    distance: "60px",
+    delay: 500,
+    reset: false
+})
+
+ScrollReveal().reveal('.tabelaRevelar', {
+    origin: "bottom",
+    distance: "60px",
+    delay: 500,
+    reset: false
+})
+
+ScrollReveal().reveal('#menu', {
+    origin: "top",
+    distance: "60px",
+    delay: 500,
+    reset: false
+})
